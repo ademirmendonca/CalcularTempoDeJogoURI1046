6c485d33596daf1b3b00510070bec96f03b12d28
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-05-07 18:25:29 -0300

    Add project files.

---
+++ CalcularTempoDeJogoURI1046/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CalcularTempoDeJogoURI1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int inicio, fim;
+            inicio = int.Parse(vet[0]);
+            fim = int.Parse(vet[1]);
+
+            if (inicio > fim)
+            {
+                Console.WriteLine("O JOGO DUROU " + (( fim + 24) - inicio ) + " HORA(S)");
+            }
+
+            else if (inicio < fim)
+            {
+                Console.WriteLine("O JOGO DUROU " + Math.Abs(inicio - fim) + " HORA(S)");
+            }
+
+            else if (inicio == fim)
+            {
+                Console.WriteLine("O JOGO DUROU 24 HORA(S)");
+            }
+
+        }
+    }
+}
